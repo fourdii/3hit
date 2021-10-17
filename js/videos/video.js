d43491fb7271cce246c544118308de91ea509e02
Feng <feng.chen@xrspace.io>
2021-10-17 15:33:43 +0800

    update pages

---
+++ js/videos/video.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./styles.css";
+import App from "./videoPage.js";
+
+
+
+ReactDOM.render(
+    <App />,
+  document.getElementById("root")
+);
+
+

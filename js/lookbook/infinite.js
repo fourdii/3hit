5648da176740c39e9fd056e9c3ca07797b4af357
Feng <feng.chen@xrspace.io>
2021-10-17 15:20:53 +0800

    update

--- js/lookbook/infinite.js
+++ js/lookbook/infinite.js
@@ -1,9 +1,9 @@
-import One from './demo-1.js'
+import App from './demo-1.js'
 
 // const demos = [One]
 // const demo = document.body.getAttribute('data-id')
 
-new One()
+new App()
 
 document.documentElement.classList.remove('no-js')
 document.documentElement.classList.add('js')
